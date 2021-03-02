1fe15d9105fad0921d2128fdd3f342c8b0c5d324
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-03-03 03:51:37 +0900

    navigate to biometric

---
+++ app/src/main/java/com/example/androidtechsample/PlaygroundFragment.kt
@@ -0,0 +1,31 @@
+package com.example.androidtechsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.androidtechsample.databinding.FragmentPlaygroundBinding
+
+class PlaygroundFragment : Fragment() {
+
+    private lateinit var binding: FragmentPlaygroundBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlaygroundBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonPlaygroundToDesignMock.setOnClickListener {
+            findNavController().navigate(R.id.to_fragment_biometric)
+        }
+    }
+}
