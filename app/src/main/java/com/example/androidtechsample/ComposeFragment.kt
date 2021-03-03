8867ce20d59648d849bc45e1c86b3947739dbdf2
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-03-04 00:50:31 +0900

    basic compose

--- app/src/main/java/com/example/androidtechsample/ComposeFragment.kt
+++ app/src/main/java/com/example/androidtechsample/ComposeFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
@@ -37,10 +45,14 @@ class ComposeFragment : Fragment() {
     @Composable
     fun NewsStory() {
         val image = painterResource(R.drawable.header)
+        val counterState = remember { mutableStateOf(0) }
+        val names = List(1000) { "Hello Android #$it" }
         MaterialTheme {
             val typography = MaterialTheme.typography
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 val imageModifier = Modifier
                     .requiredHeight(180.dp)
@@ -62,16 +74,86 @@ class ComposeFragment : Fragment() {
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
-                Text(
-                    "Davenport, California",
-                    style = typography.body2
+                Surface(color = Color.Yellow) {
+                    Text(
+                        "Davenport, California",
+                        style = typography.body2,
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .fillMaxWidth()
+                    )
+                }
+                Divider(
+                    color = Color.Black,
+                    thickness = 4.dp
                 )
                 Text(
                     "December 2018",
                     style = typography.body1
                 )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Counter()
+                    Counter2(
+                        count = counterState.value,
+                        updateCount = {
+                            counterState.value = it
+                        }
+                    )
+                }
+                NameList(names, Modifier.weight(1f))
             }
         }
+    }
+
+    @Composable
+    fun Counter() {
+        val count = remember { mutableStateOf(0) }
+        Button(onClick = { count.value++ }) {
+            Text("I've been clicked ${count.value} times")
+        }
+    }
+
+    @Composable
+    fun Counter2(count: Int, updateCount: (Int) -> Unit) {
+        Button(
+            onClick = { updateCount(count + 1) },
+            colors = ButtonDefaults.buttonColors(backgroundColor = if (count > 5) Color.Green else Color.White)
+        ) {
+            Text("I've been clicked $count times")
+        }
+    }
+
+    @Composable
+    fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(items = names) { name ->
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+    }
+
+    @Composable
+    fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        var isSelected = remember { mutableStateOf(false) }
+        val backgroundColor = animateColorAsState(if (isSelected.value) Color.Red else Color.Transparent)
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .padding(24.dp)
+                .background(color = backgroundColor.value)
+                .clickable(onClick = {isSelected.value = !isSelected.value})
+        )
     }
 
     @Preview
