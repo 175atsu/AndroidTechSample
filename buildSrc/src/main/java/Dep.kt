df290b1b1d84e306390420b8f18d813489d09b8c
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-04-18 21:23:07 +0900

    add buildSrc

---
+++ buildSrc/src/main/java/Dep.kt
@@ -0,0 +1,46 @@
+package com.example.buildsrc
+
+object Versions {
+    const val kotlin = "1.4.32"
+    const val navigation = "2.3.5"
+}
+
+object Dep {
+
+    object Kotlin {
+        const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        const val jdk = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+        const val allOpen = "org.jetbrains.kotlin:kotlin-allopen:${Versions.kotlin}"
+    }
+
+    object Jetpack {
+        const val appcompat = "androidx.appcompat:appcompat:1.3.0-beta01"
+        const val startup = "androidx.startup:startup-runtime:1.0.0"
+        const val browser = "androidx.browser:browser:1.3.0"
+        const val dataStore = "androidx.datastore:datastore-preferences:1.0.0-alpha06"
+    }
+
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:1.2.0"
+        const val core = "androidx.core:core-ktx:1.5.0-beta01"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+        const val navUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+        const val navRuntime = "androidx.navigation:navigation-runtime-ktx:${Versions.navigation}"
+        const val navFragmentKtx =
+            "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+        const val plugin =
+            "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+        const val biometric = "androidx.biometric:biometric:1.1.0"
+        object Test {
+            const val ext = "androidx.test.ext:junit:1.1.2"
+            const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+        }
+    }
+
+    const val material = "com.google.android.material:material:1.3.0"
+
+    object Test {
+        const val junit4 = "junit:junit:4.13.2"
+    }
+}
