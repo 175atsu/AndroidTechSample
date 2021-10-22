594c52c1813cedad58d0d6f19c738e945de0545b
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-10-23 01:43:57 +0900

    create view

---
+++ app/src/main/java/com/example/androidtechsample/ui/compose/ComposeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.androidtechsample.ui.compose
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ComposeFragment : Fragment() {
+}
