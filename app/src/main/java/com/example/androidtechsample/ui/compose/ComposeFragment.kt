0001d0d802e575ad79ed1537270fdb0f76af8e32
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-10-24 03:50:49 +0900

    create nav

--- app/src/main/java/com/example/androidtechsample/ui/compose/ComposeFragment.kt
+++ app/src/main/java/com/example/androidtechsample/ui/compose/ComposeFragment.kt
@@ -4,24 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import com.example.androidtechsample.R
-import com.example.androidtechsample.component.TextBlackHead1
-import com.example.androidtechsample.component.TextBlackHead2
-import com.example.androidtechsample.component.TextWhiteBody1
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.androidtechsample.ui.compose.MainDestinations.PLAYGROUND_ROUTE
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,45 +22,19 @@ class ComposeFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View = ComposeView(requireContext()).apply {
     setContent {
-      CreateView()
+      CreateNav()
     }
   }
 }
 
-@Composable
-fun CreateView() {
-  MaterialTheme {
-    Column(
-      modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-    ) {
-      TextBlackHead1(
-        text = stringResource(id = R.string.compose_playground),
-        modifier = Modifier.padding(vertical = 8.dp)
-      )
-      Spacer(Modifier.height(16.dp))
-      TextBlackHead2(
-        text = stringResource(id = R.string.compose_test)
-      )
-      ToPageButton(toPage = { /*TODO*/ }, textResource = R.string.compose_new_page)
-    }
-  }
+object MainDestinations {
+  const val PLAYGROUND_ROUTE = "playground"
 }
 
 @Composable
-fun ToPageButton(
-  toPage: () -> Unit,
-  textResource: Int,
-) {
-  Button(
-    onClick = toPage,
-    shape = RoundedCornerShape(8.dp)
-  ) {
-    TextWhiteBody1(stringResource(textResource))
+fun CreateNav(startDestination: String = PLAYGROUND_ROUTE) {
+  val navController = rememberNavController()
+  NavHost(navController = navController, startDestination = startDestination) {
+    composable(PLAYGROUND_ROUTE) { PlaygroundScreen() }
   }
-}
-
-@Preview
-@Composable
-fun Preview() {
-  CreateView()
 }
