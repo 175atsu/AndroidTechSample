b0fc8f9722905e2a1b9bd82c800860b1a72adb34
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-10-29 00:49:07 +0900

    delete

--- feature/biometric/build.gradle.kts
+++ feature/biometric/build.gradle.kts
@@ -8,21 +8,6 @@ plugins {
 
 android {
   compileSdk = 31
-  buildToolsVersion = "30.0.3"
-
-  defaultConfig {
-    minSdk = 23
-    targetSdk = 31
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
   buildFeatures {
     dataBinding = true
   }
--- feature/resources/build.gradle.kts
+++ feature/resources/build.gradle.kts
@@ -5,14 +5,6 @@ plugins {
 
 android {
   compileSdk = 31
-
-  defaultConfig {
-    minSdk = 21
-    targetSdk = 31
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
 }
 
 dependencies {
