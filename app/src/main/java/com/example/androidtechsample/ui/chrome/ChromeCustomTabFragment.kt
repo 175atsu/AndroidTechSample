30c9e619313ff0b617eb59b04e3c425417a502b0
Iino <astu.i.0601@gmail.com>
2023-02-26 23:23:45 +0900

    fix code

--- app/src/main/java/com/example/androidtechsample/ui/chrome/ChromeCustomTabFragment.kt
+++ app/src/main/java/com/example/androidtechsample/ui/chrome/ChromeCustomTabFragment.kt
@@ -1,14 +1,11 @@
 package com.example.androidtechsample.ui.chrome
 
-import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import com.example.androidtechsample.databinding.FragmentChromeCustomTabBinding
@@ -21,28 +18,8 @@ class ChromeCustomTabFragment : Fragment() {
 
   private lateinit var binding: FragmentChromeCustomTabBinding
 
-  private val activityResultLauncher: ActivityResultLauncher<String> =
-    registerForActivityResult(object :
-      ActivityResultContract<String, Int>() {
-      override fun createIntent(
-        context: Context,
-        input: String
-      ): Intent {
-        val builder = CustomTabsIntent.Builder()
-          .setInitialActivityHeightPx(800)
-          .setToolbarCornerRadiusDp(16)
-        val customTabsIntent = builder.build().intent
-        customTabsIntent.data = Uri.parse(input)
-        return customTabsIntent
-      }
-
-      override fun parseResult(
-        resultCode: Int,
-        intent: Intent?
-      ): Int {
-        return resultCode
-      }
-    }) { resultCode ->
+  private val startForResult =
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
       // 終了時に呼び出されるコールバック
     }
 
@@ -58,7 +35,12 @@ class ChromeCustomTabFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.openWebView.setOnClickListener {
-      activityResultLauncher.launch(URL)
+
+      val builder = CustomTabsIntent.Builder()
+        .setInitialActivityHeightPx(1000)
+        .setToolbarCornerRadiusDp(16)
+      val customTabsIntent = builder.build().intent.apply { data = Uri.parse(URL) }
+      startForResult.launch(customTabsIntent)
     }
   }
 }
