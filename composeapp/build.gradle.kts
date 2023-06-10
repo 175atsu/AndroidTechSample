9c0a7850928fd07ab19f9a74934917323af28ec5
Iino <astu.i.0601@gmail.com>
2023-06-10 22:52:54 +0900

    fix gradlew kts

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -46,9 +46,9 @@ android {
 
 dependencies {
 
-  implementation("androidx.core:core-ktx:1.8.0")
-  implementation("androidx.appcompat:appcompat:1.4.1")
-  implementation("com.google.android.material:material:1.5.0")
+  implementation(Dep.AndroidX.core)
+  implementation(Dep.AndroidX.appCompat)
+  implementation(Dep.material)
 
   implementation(Dep.AndroidX.Compose.ui)
   implementation(Dep.AndroidX.Compose.navigation)
