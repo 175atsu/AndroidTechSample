845c754d7ef324e83eac8198cad5c24b240fad16
Iino <astu.i.0601@gmail.com>
2024-01-05 13:51:18 +0900

    create ContentsCard

--- core/designsystem/src/main/java/com/messi/designsystem/digitalagency/component/DigitalAgencyTopAppbar.kt
+++ core/designsystem/src/main/java/com/messi/designsystem/digitalagency/component/DigitalAgencyTopAppbar.kt
@@ -5,10 +5,12 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -22,6 +24,7 @@ fun DigitalAgencyTopAppBar(
   contentDescription: String? = null,
   @DrawableRes navigationIcon: Int? = null,
   onClickNavigationIcon: () -> Unit = {},
+  actions: @Composable RowScope.() -> Unit = {},
 ) {
   DigitalAgencyTopAppBar(
     title = {
@@ -41,14 +44,16 @@ fun DigitalAgencyTopAppBar(
         }
       }
     },
+    actions = actions,
   )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DigitalAgencyTopAppBar(
   title: @Composable () -> Unit,
   modifier: Modifier = Modifier,
-  navigationIcon: @Composable (() -> Unit)? = null,
+  navigationIcon: @Composable () -> Unit = {},
   actions: @Composable RowScope.() -> Unit = {},
 ) {
   TopAppBar(
@@ -56,8 +61,8 @@ fun DigitalAgencyTopAppBar(
     modifier = modifier.statusBarsPadding(),
     navigationIcon = navigationIcon,
     actions = actions,
-    backgroundColor = DigitalAgencyTheme.colors.backgroundPrimary,
-    contentColor = DigitalAgencyTheme.colors.textBody,
-    elevation = 0.dp,
+    colors = TopAppBarDefaults.topAppBarColors(
+      containerColor = DigitalAgencyTheme.colors.backgroundPrimary
+    ),
   )
 }
